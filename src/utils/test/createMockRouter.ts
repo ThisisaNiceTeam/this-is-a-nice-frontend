265b94d3fce7fefaf8064f60a06fa6eb83728170
minjj0905 <minjj0905@daum.net>
2023-10-11 18:04:01 +0900

    feat: 테스트 유틸 함수 작성
    next router 사용 컴포넌트를 위한 mockRouter, mockRouter 렌더링 함수

---
+++ src/utils/test/createMockRouter.ts
@@ -0,0 +1,29 @@
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
+
+export function createMockRouter(router?: Partial<AppRouterInstance>): any {
+  return {
+    basePath: '',
+    pathname: '/',
+    route: '/',
+    query: {},
+    asPath: '/',
+    back: jest.fn(),
+    beforePopState: jest.fn(),
+    prefetch: jest.fn(),
+    push: jest.fn(),
+    reload: jest.fn(),
+    replace: jest.fn(),
+    events: {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+    },
+    isFallback: false,
+    isLocaleDomain: false,
+    isReady: true,
+    defaultLocale: 'en',
+    domainLocales: [],
+    isPreview: false,
+    ...router,
+  };
+}
