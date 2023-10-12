66205fd112a669d3b90db43998972b546301d000
KIM MIN JEONG <minjj0905@daum.net>
2023-10-12 15:13:09 +0900

    테스팅 환경 설정 및 샘플 코드 작성 (#15)
    * chore: jest & testing library setting
    
    * feat: 테스트 유틸 함수 작성
    
    next router 사용 컴포넌트를 위한 mockRouter, mockRouter 렌더링 함수
    
    * test: HomeButton 테스트 코드 작성
    
    * test: 테스트 상세 수정, test util 인덱싱
    
    * test: 코드 가독성 개선
    
    * pre-commit 설정 (#16)
    
    * chore: pre-commit setting
    
    * fix: eol

---
+++ src/components/HomeButton.spec.tsx
@@ -0,0 +1,29 @@
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+
+import { renderWithContext, createMockRouter } from '@/utils/test';
+
+import HomeButton from './HomeButton';
+
+describe('HomeButton', () => {
+  const router = createMockRouter();
+  const render = () => renderWithContext(<HomeButton />, router);
+
+  it('render Test', () => {
+    render();
+
+    const logo = screen.getByAltText('logo');
+    const title = screen.getByText('사이트 이름');
+
+    expect(logo).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+  });
+
+  it('클릭하면 홈으로 이동한다.', async () => {
+    render();
+
+    await userEvent.click(screen.getByRole('button'));
+
+    expect(router.push).toBeCalledWith('/');
+  });
+});
---
+++ src/utils/test/renderWithContext.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { AppRouterContext, AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
+
+import createMockRouter from './createMockRouter';
+
+function renderWithContext(component: JSX.Element, router: AppRouterInstance = createMockRouter()) {
+  return render(<AppRouterContext.Provider value={router}>{component}</AppRouterContext.Provider>);
+}
+
+export default renderWithContext;
