f49cdf776d61a3007495c1338f7d926e1466dc8e
Asher Seo <beomxtone@gmail.com>
2023-10-14 18:42:55 +0900

    test: 테마 버튼 테스트 코드 변경

--- src/components/ThemedButton.spec.tsx
+++ src/components/ThemedButton.spec.tsx
@@ -6,21 +6,23 @@ import ThemedButton from './ThemedButton';
 
 describe('ThemedButton Component', () => {
   render(<ThemedButton />);
-  const darkModeIcon = screen.getByTestId('dark-button');
+  const lightModeIcon = screen.getByTestId('light-button');
 
-  it('초기 버튼은 dark mode 버튼이다.', () => {
-    expect(darkModeIcon).toBeInTheDocument();
+  it('초기 버튼은 light mode 버튼이다.', () => {
+    expect(lightModeIcon).toBeInTheDocument();
   });
 
   it('각 버튼을 클릭하면 반대 버튼으로 바뀐다.', async () => {
     setTimeout(() => {
-      const lightModeIcon = screen.getByTestId('light-button');
-      expect(lightModeIcon).toBeInTheDocument();
+      const darkModeIcon = screen.getByTestId('dark-button');
+      expect(darkModeIcon).toBeInTheDocument();
+      expect(lightModeIcon).not.toBeInTheDocument();
 
-      fireEvent.click(lightModeIcon);
+      fireEvent.click(darkModeIcon);
 
       setTimeout(() => {
-        expect(darkModeIcon).toBeInTheDocument();
+        expect(lightModeIcon).toBeInTheDocument();
+        expect(darkModeIcon).not.toBeInTheDocument();
       }, 10);
     }, 10);
   });
