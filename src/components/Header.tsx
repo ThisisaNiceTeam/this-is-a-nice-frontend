d6f86a3859aa2bdf306f38d1960f3bd899d68122
Asher Seo <beomxtone@gmail.com>
2023-10-23 17:34:54 +0900

    style: 홈 화면 반응형 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,18 @@ import HomeButton from '@/components/HomeButton';
 import ThemedButton from '@/components/ThemedButton';
 
 const HeaderWrapper = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 60px;
   position: fixed;
   background-color: var(--bg-home);
   color: var(--text-primary);
   display: flex;
   justify-content: center;
+  z-index: 10;
 `;
 
 const HeaderContent = styled.header`
-  width: 100vw;
+  width: 100%;
   height: 60px;
   display: flex;
   justify-content: space-around;
