60582c2c07e5f1bf29d4b51e19c4eb44bd0a8174
Asher Seo <beomxtone@gmail.com>
2023-10-30 12:11:10 +0900

    style: 홈 화면 반응형 구현 (#24)

--- src/containers/home/Content.tsx
+++ src/containers/home/Content.tsx
@@ -12,32 +12,45 @@ const HomeElement = styled.div`
 const TitleWrapper = styled.div`
   height: calc(100vh - 60px);
   background-image: linear-gradient(to top, var(--bg-gradient), transparent);
+  position: relative;
+  overflow: hidden;
 `;
 
 const Title = styled.div`
-  padding-top: 7rem;
+  padding-top: 10vh;
   text-align: center;
-  font-size: 4.125rem;
+  font-size: 2rem;
+  @media (min-width: 600px) {
+    font-size: 4.125rem;
+  }
   font-weight: bold;
 `;
 
 const ChatWrapper = styled.div`
+  position: relative;
   width: 50vw;
-  min-width: 300px;
-  max-width: 800px;
-  height: 25rem;
+  height: 25vh;
   margin: 3rem auto;
   text-align: center;
   font-weight: 600;
+  min-width: 200px;
+  @media (min-width: 600px) {
+    max-width: 600px;
+    min-width: 400px;
+  }
 `;
 
 const Chat = styled.div`
   width: fit-content;
   margin: 1rem;
-  padding: 0.625rem 1.2rem;
-  font-size: 2.5rem;
   border: 0.05rem solid var(--outline-primary);
   border-radius: 2rem;
+  font-size: 1.25rem;
+  padding: 0.3rem 0.6rem;
+  @media (min-width: 600px) {
+    font-size: 2.5rem;
+    padding: 0.625rem 1.2rem;
+  }
 `;
 const ChatOther = styled(Chat)`
   background-color: var(--bg-home);
@@ -54,20 +67,42 @@ const ChatMine = styled(Chat)`
 const ChatAlert = styled.div`
   margin-bottom: 2rem;
   color: var(--text-secondary);
-  font-size: 1.5rem;
+  font-size: 0.75rem;
+  @media (min-width: 600px) {
+    font-size: 1.5rem;
+  }
 `;
 
-const StartButton = styled.div`
+const Button = styled.button`
+  display: flex;
   width: fit-content;
-  padding: 1rem 2rem;
-  margin: 3rem auto;
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+  align-items: center;
+`;
+
+const StartButton = styled(Button)`
+  padding: 0.5rem 1rem;
+  margin: 0 auto 1rem;
   background-color: #202026;
   color: white;
   border: 0.05rem solid var(--outline-primary);
   border-radius: 2rem;
-  font-size: 2rem;
+  font-size: 1rem;
   font-weight: bold;
-  cursor: pointer;
+
+  @media (min-width: 600px) {
+    padding: 1rem 2rem;
+    font-size: 2rem;
+  }
+`;
+
+const MoreButton = styled(Button)`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
 `;
 
 const HomeContent = () => {
@@ -85,8 +120,11 @@ const HomeContent = () => {
           <ChatMine>ㄴㅇㅊ</ChatMine>
           <ChatAlert>상대가 나갔습니다.</ChatAlert>
           <StartButton>시작하기</StartButton>
-          <ExpandMore width={50} />
         </ChatWrapper>
+
+        <MoreButton>
+          <ExpandMore width={50} />
+        </MoreButton>
       </TitleWrapper>
     </HomeElement>
   );
