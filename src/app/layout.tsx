b7db8528dcd92357afc12dd5430ec4d3ebf4044a
Asher Seo <beomxtone@gmail.com>
2023-12-19 02:29:58 +0900

    feat: 카카오 Script 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Script from 'next/script';
 
 import type { Metadata } from 'next';
 
@@ -17,6 +18,11 @@ const RootLayout = ({ children }: Props) => {
   return (
     <html lang='en'>
       <body>
+        <Script
+          src='https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js'
+          integrity='sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8'
+          crossOrigin='anonymous'
+        />
         <Provider>{children}</Provider>
       </body>
     </html>
